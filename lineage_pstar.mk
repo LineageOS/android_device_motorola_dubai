92f5a6c44a84c1dad7516a48634d8f514bf1782d
SGCMarkus <markusornik@gmail.com>
2022-02-15 16:35:31 +0100

    pstar: setup pstar as a device

---
+++ lineage_pstar.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from pstar device
+$(call inherit-product, device/motorola/pstar/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_pstar
+PRODUCT_DEVICE := pstar
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := XT2153-1
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=pstar \
+    PRODUCT_NAME=pstar_retail \
+    PRIVATE_BUILD_DESC="pstar_retail-user 11 RRA31.Q3-19-19 d30cf release-keys"
+
+BUILD_FINGERPRINT := motorola/pstar_retail/pstar:11/RRA31.Q3-19-19/d30cf:user/release-keys
