52c029756bd1c076fe2fbbe9d4e18f52395976f6
SGCMarkus <markusornik@gmail.com>
2022-02-15 19:08:49 +0100

    pstar: add device specific audio configs

--- device.mk
+++ device.mk
@@ -23,7 +23,12 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 # Inherit from motorola sm8250-common
 $(call inherit-product, device/motorola/sm8250-common/common.mk)
 
-
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/pstar/pstar-vendor.mk)
