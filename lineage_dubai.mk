8182a18ba7b431fd09c8bc818a1f54880f5c3768
sb6596 <shubhamprince111@gmail.com>
2022-11-12 19:15:27 +0000

    dubai: Update to S1RD32M.55-94
    While we're at it, remove the duplicated camera sensormodule section that
    is already included in the camera sensor section.
    
    Change-Id: I9c78b3e2268c627494eb70a2d862b88161e56b36
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>
    Co-authored-by: Marc Bourgoin <themard85@gmail.com>

--- lineage_dubai.mk
+++ lineage_dubai.mk
@@ -33,7 +33,7 @@ PRODUCT_MODEL := motorola edge 30
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_PRODUCT=dubai_retail \
-    PRIVATE_BUILD_DESC="dubai_g-user 12 S1RD32M.55-67 356bc release-keys"
+    TARGET_PRODUCT=dubai_g \
+    PRIVATE_BUILD_DESC="dubai_g-user 12 S1RD32M.55-94 9be6b release-keys"
 
-BUILD_FINGERPRINT := motorola/dubai_g/dubai:12/S1RD32M.55-67/356bc:user/release-keys
+BUILD_FINGERPRINT := motorola/dubai_g/dubai:12/S1RD32M.55-94/9be6b:user/release-keys
