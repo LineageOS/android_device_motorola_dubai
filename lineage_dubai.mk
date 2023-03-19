343ad619d7b7f7940875cfd790ae4a9a8f331c7c
Marc Bourgoin <themard85@gmail.com>
2023-03-19 01:18:48 -0600

    dubai: Update blobs from DUBAI_RETAIL_12_S1RDS32.55-106-3_subsidy-DEFAULT_regulatory-DEFAULT_cid50_CFC
    Change-Id: Ifc7250282a3cce0d6346da7526769bc79403c6e0

--- lineage_dubai.mk
+++ lineage_dubai.mk
@@ -34,6 +34,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=dubai_g \
-    PRIVATE_BUILD_DESC="dubai_g-user 12 S1RD32.55-106 0cdff5-e7305 release-keys"
+    PRIVATE_BUILD_DESC="dubai_g-user 12 S1RD32.55-106-3 98ea12-d3640 release-keys"
 
-BUILD_FINGERPRINT := motorola/dubai_g/dubai:12/S1RD32.55-106/0cdff5-e7305:user/release-keys
+BUILD_FINGERPRINT := motorola/dubai_g/dubai:12/S1RD32.55-106-3/98ea12-d3640:user/release-keys
