06cc22da0ac851b41f602afb41f0098f439ec454
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-04-05 17:26:29 +0530

    dubai: Add support for high brightness mode (HBM)
    Brightness from 3514 (600 nits) to 4095 (700 nits) is HBM.
    Enter HBM at >=6500 lux (outdoor daylight) and hit peak brightness
    at >=10000 lux (under direct sunlight). Adjust the backlight curve
    accordingly.
    
    Requires increasing max brightness to 4095 in kernel.
    
    Change-Id: If39072cadf21428e8f372a5264bf33c20e8bef55

--- device.mk
+++ device.mk
@@ -51,6 +51,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Display
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display_id_4630947043778501762.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947043778501762.xml
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.dubai
