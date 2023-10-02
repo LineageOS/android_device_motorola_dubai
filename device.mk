c74c687c0541b750631f9f2d218976221beb2f62
Marc Bourgoin <themard85@gmail.com>
2023-10-02 14:28:58 -0600

    dubai: Swap thermal config for dubai-game-perf
    The default thermal-engine-dubai.conf is way too agressive with throttling that the performance
    is way too low for any users trying to play 3D games on a Snapdragon 778G+ runs slower than
    a SD 778G, so let's swap to the dubai-game-perf config as when we compared the configs it
    allows for a better performance to thermal balance. We also checked dubai-perf but that one
    was more of a "let's get the highest scores in a benchmark and turn the device into a
    handwarmer" kind of thermal config.
    
    Change-Id: Id5c991a198ef6be61009a9785b5e8604aa1fdc91

--- device.mk
+++ device.mk
@@ -99,7 +99,7 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-dubai.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-dubai.conf
+    $(LOCAL_PATH)/configs/thermal-engine-dubai-game-perf.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-dubai.conf
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/dubai/dubai-vendor.mk)
