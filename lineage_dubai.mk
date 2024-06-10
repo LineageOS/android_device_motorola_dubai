19c2e47323eb80a121f170f6807404a61089330f
Marc Bourgoin <themard85@gmail.com>
2024-06-09 23:23:17 -0600

    dubai: Update blobs from from dubai_g-user 13 T1RDS33.116-33-15-6 e089e7-b95537 release-keys - RETAIL variant
    Change-Id: Id1418267fb9e0dcbddb9efe57dc66657675cec23

--- lineage_dubai.mk
+++ lineage_dubai.mk
@@ -24,6 +24,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=dubai_g \
-    PRIVATE_BUILD_DESC="dubai_g-user 13 T1RDS33.116-33-15-1 06f6d-c0e7b release-keys"
+    PRIVATE_BUILD_DESC="dubai_g-user 13 T1RDS33.116-33-15-6 e089e7-b95537 release-keys"
 
-BUILD_FINGERPRINT := motorola/dubai_g/dubai:13/T1RDS33.116-33-15-1/06f6d-c0e7b:user/release-keys
+BUILD_FINGERPRINT := motorola/dubai_g/dubai:13/T1RDS33.116-33-15-6/e089e7-b95537:user/release-keys
