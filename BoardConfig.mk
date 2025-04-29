2250a5e53302ed6f9a6b0e4755d87c4aa74410a1
Bruno Martins <bgcngm@gmail.com>
2025-04-30 00:00:08 +0100

    dubai: Set UDFPS extension lib via soong config
    Change-Id: I8a709bde4843b61420ea1003a749952d2619e2da

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,6 @@ include device/motorola/sm7325-common/BoardConfigCommon.mk
 TARGET_BOOTLOADER_BOARD_NAME := dubai
 
 # Fingerprint
-TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.dubai
 SOONG_CONFIG_qtidisplay_udfps := true
 
 # HIDL
