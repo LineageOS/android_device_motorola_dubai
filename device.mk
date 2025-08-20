0d626226e0116c24ba5220014b199ae599d948e8
LuK1337 <priv.luk@gmail.com>
2025-08-20 19:20:35 -0400

    dubai: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- device.mk
+++ device.mk
@@ -65,6 +65,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/goodix_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/goodix_ts.kl
 
+# LiveDisplay
+$(call soong_config_set,livedisplay_motorola,enable_af,true)
+$(call soong_config_set,livedisplay_motorola,enable_se,true)
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service.st \
