ad4862b2f0d724cade3736bee15f6a14b76d60dd
Marc Bourgoin <themard85@gmail.com>
2025-12-11 02:24:56 -0700

    fixup! dubai: Pin MotCamera4
    Change-Id: I0463e76227b428f57921e408095e1b723b8a2dec

--- extract-files.py
+++ extract-files.py
@@ -35,8 +35,6 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'product/priv-app/MotCamera4/MotCamera4.apk': blob_fixup()
-        .apktool_patch('MotCamera4-patches'),
     ('vendor/lib/libmot_chi_desktop_helper.so', 'vendor/lib64/libmot_chi_desktop_helper.so'): blob_fixup()
         .add_needed('libgui_shim_vendor.so'),
     'vendor/lib64/sensors.moto.so': blob_fixup()
