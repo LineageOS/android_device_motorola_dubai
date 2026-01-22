29e3fa6d18b81002147ef76d3c88313929fad6f0
Nick Reuter <nreuter85@gmail.com>
2026-01-21 20:53:29 -0600

    dubai: Switch to hardware/motorola fingerprint
    Change-Id: I7f2b1eca23e740cbdbf1fe84bb878a600582a17f

--- device.mk
+++ device.mk
@@ -47,7 +47,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.dubai
+    android.hardware.biometrics.fingerprint@2.3-service.moto
 
 $(call soong_config_set,surfaceflinger,udfps_lib,//$(LOCAL_PATH):libudfps_extension.dubai)
 
