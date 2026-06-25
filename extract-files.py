ab0ba3434114cdac0e4bcc70eb42ecba8c9d5230
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-25 18:54:55 -0400

    dubai: Use libgui_buffer_shim for necessary libs
    * libmot_chi_desktop_helper.so wants pre-refactor 3-arg
      `acquireBuffer(BufferItem*, nsecs_t, bool)` and 2-arg
      `releaseBuffer(const BufferItem&, const sp<Fence>&)`.
    * Sadly this breaks 32-bit build, which /some/ people might
      be using, so add a dedicated target for this that devices
      can link.
    
    Change-Id: I1d61678ae7a5b05ddbbb58281e875a575d94d1cb

--- extract-files.py
+++ extract-files.py
@@ -36,8 +36,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    ('vendor/lib/libmot_chi_desktop_helper.so', 'vendor/lib64/libmot_chi_desktop_helper.so'): blob_fixup()
-        .add_needed('libgui_shim_vendor.so'),
+    'vendor/lib64/libmot_chi_desktop_helper.so': blob_fixup()
+        .add_needed('libgui_buffer_shim_vendor.so'),
     'vendor/lib64/sensors.moto.so': blob_fixup()
         .add_needed('libbase_shim.so'),
 }  # fmt: skip
