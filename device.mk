b9ee7ef8b22402ac5e5e83f59a1b640dd9f1197c
Marc Bourgoin <themard85@gmail.com>
2026-08-16 21:56:47 -0600

    dubai: Add Motorola touch AIDL for HTPR
    Change-Id: Ia5764dd64b47de833fd3b3a3f94c144b37e31bbf

--- device.mk
+++ device.mk
@@ -86,5 +86,11 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-dubai-game-perf.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-dubai.conf
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch-service.motorola
+
+$(call soong_config_set, MOTOROLA_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/class/touchscreen/primary/interpolation)
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/dubai/dubai-vendor.mk)
