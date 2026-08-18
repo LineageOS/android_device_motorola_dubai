ded3aad4156f1adf8ac841352d67f54cf5cfc8ca
Marc Bourgoin <themard85@gmail.com>
2026-08-18 13:47:24 -0600

    dubai: Move thermal-engine-dubai-game-perf rename to vendor repo
    We aren't doing any extra modifications to it, just a rename
    
    Change-Id: I2d5c77739b0fb69910545c7aa3d1e8619febc1ff

--- device.mk
+++ device.mk
@@ -82,9 +82,5 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-dubai-game-perf.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-dubai.conf
-
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/dubai/dubai-vendor.mk)
